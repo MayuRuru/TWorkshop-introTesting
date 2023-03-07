3f705f62e61da72bf022e937a4b0e4fa70905794
MayuRuru <aruizrull@protonmail.com>
2023-03-07 14:28:41 +0100

    Finish part 3

--- introTestingJS/account.js
+++ introTestingJS/account.js
@@ -1,6 +1,17 @@
 class Account {
+  constructor() {
+    this.amount = 0;
+  }
+
   getAmount() {
-    return 0;
+    return this.amount;
+  }
+
+  setAmount(value) {
+    if (isNaN(value)) {
+      throw new Error("not a number");
+    }
+    this.amount = value;
   }
 }
 
--- introTestingJS/account.test.js
+++ introTestingJS/account.test.js
@@ -7,3 +7,22 @@ test("Given I have a new account, When I call getAmount(), Then it returns 0", (
 
   expect(value).toBe(0);
 });
+
+test("Given I have a new account, When I call setAmount with 10, Then getAmount should return 10", () => {
+  testee = new Account();
+
+  testee.setAmount(10);
+
+  expect(testee.getAmount()).toBe(10);
+});
+
+test("Given I have a new account, When I set value that is not a number, Then it should throw and error", () => {
+  testee = new Account();
+
+  // to throw an error we have to write it inside a function to do a try-catch:
+  action = () => {
+    testee.setAmount("not a number");
+  };
+
+  expect(action).toThrow();
+});
